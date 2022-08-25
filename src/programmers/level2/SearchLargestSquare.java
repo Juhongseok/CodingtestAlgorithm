68aca7fb511bc3664daafd7b9f64b4e7ea0066dc
Juhongseok <wnghdtjr129@naver.com>
2022-08-25 16:13:39 +0900

    [PGS] 가장 큰 정사각형 찾기 /Level2/30분

---
+++ src/programmers/level2/SearchLargestSquare.java
@@ -0,0 +1,31 @@
+package programmers.level2;
+
+public class SearchLargestSquare {
+
+    public static class Solution {
+        public static int solution(int[][] board) {
+            int[][] map = new int[board.length + 1][board[0].length + 1];
+
+            int maxLen = 0;
+            for (int i = 1; i <= board.length; i++) {
+                for (int j = 1; j <= board[0].length; j++) {
+                    if(board[i-1][j-1] != 0) {
+                        int min = Math.min(Math.min(map[i - 1][j], map[i][j - 1]), map[i - 1][j - 1]);
+                        map[i][j] = min + 1;
+                        maxLen = Math.max(maxLen, min + 1);
+                    }
+                }
+            }
+
+            return maxLen * maxLen;
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] board = {{0, 1, 1, 1}, 
+                         {1, 1, 1, 1}, 
+                         {1, 1, 1, 1}, 
+                         {0, 0, 1, 0}};
+        System.out.println(Solution.solution(board));
+    }
+}
