a133e754e9ead794565a4235f5da2906e5cc0194
JHS <wnghdtjr129@naver.com>
2022-09-06 10:06:34 +0900

    [BoJ] 평범한 배낭/Gold5/30분/DP

---
+++ src/baekjoon/gold/NormalBackpack.java
@@ -0,0 +1,32 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NormalBackpack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] weight = new int[n+1];
+        int[] value = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(reader.readLine(), " ");
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] cache = new int[k+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = k; j >= weight[i]; j--) {
+                cache[j] = Math.max(cache[j], cache[j - weight[i]] + value[i]);
+            }
+        }
+        System.out.println(cache[k]);
+    }
+}
