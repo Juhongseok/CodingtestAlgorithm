86684aae185f4ef6b761d0654bec3941aeb28c24
JHS <wnghdtjr129@naver.com>
2022-09-14 18:11:04 +0900

    [PGS] n^2 배열 자르기/Lv2/20분

---
+++ src/programmers/level2/Cut2DArray.java
@@ -0,0 +1,25 @@
+package programmers.level2;
+
+public class Cut2DArray {
+    static class Solution {
+        public static int[] solution(int n, long left, long right) {
+            int[] answer = new int[(int) (right - left + 1)];
+
+            int index = 0;
+            while (left <= right) {
+                answer[index++] = (int) (Math.max(Math.floor(left / n), left++ % n) + 1);
+            }
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        long left = 7;
+        long right = 14;
+
+        int[] solution = Solution.solution(n, left, right);
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
+    }
+}
